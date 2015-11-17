def4fc66d1d2eb4b6f917c0d21b29d50b0c4c658
Tero Laxström <tero.laxstrom@visit.fi>
2015-11-17 12:40:11 +0200

    Allowing more than one directive per page.
    Moved everything inside link function.
    
    Signed-off-by: Tero Laxström <tero.laxstrom@visit.fi>

--- src/main.js
+++ src/main.js
@@ -9,68 +9,68 @@
 	var aim = angular.module('tjlaxs.aim', []);
 
 	aim.directive('tjlImageMask', function() {
-		var ctx = null;
-		var canvas = null;
-		var mask = null;
-		var mouseX = 0;
-		var mouseY = 0;
-		var dirScope = null;
-		var controller;
-
-		function updateMouse(evt, element) {
-			var rect = element.getBoundingClientRect();
-			var scrollTop = document.documentElement.scrollTop ?
-					document.documentElement.scrollTop :
-					document.body.scrollTop;
-			var scrollLeft = document.documentElement.scrollLeft ?
-					document.documentElement.scrollLeft :
-					document.body.scrollLeft;
-			var elementLeft = rect.left+scrollLeft;
-			var elementTop = rect.top+scrollTop;
-
-			mouseX = evt.pageX-elementLeft;
-			mouseY = evt.pageY-elementTop;	
-		}
-
-		function draw() {
-			ctx.clearRect(0, 0, canvas.width, canvas.height);
-			mask.draw(ctx);
-		}
-
-		function mouseDownListener(evt) {
-			updateMouse(evt, canvas);
-			if(dirScope.config.mode === 'edit' && controller.startDrag(mouseX, mouseY)) {
-				canvas.addEventListener('mousemove', mouseEditMoveListener, false);
-			}
-
-			// Prevent event going further
-			if (evt.preventDefault) {
-				evt.preventDefault();
-			}
-			else if (evt.returnValue) {
-				evt.returnValue = false;
-			}
-		}
-
-		function mouseEditMoveListener(evt) {
-			if(controller.getDragging()) {
-				updateMouse(evt, canvas);
-				console.log('starting to drag');
-				controller.drag(mouseX, mouseY);
-				console.log('stopping drag and starting draw');
-				draw();
-				console.log('stopping draw');
-			}
-		}
-
-		function mouseUpListener(evt) {
-			updateMouse(evt, canvas);
-			controller.stopDrag(mouseX, mouseY);
-			canvas.removeEventListener('mousemove', mouseEditMoveListener, false);
-			dirScope.$apply();
-		}
-
 		function link(scope, element/*, attrs*/) {
+			var ctx = null;
+			var canvas = null;
+			var mask = null;
+			var mouseX = 0;
+			var mouseY = 0;
+			var dirScope = null;
+			var controller;
+	
+			function updateMouse(evt, element) {
+				var rect = element.getBoundingClientRect();
+				var scrollTop = document.documentElement.scrollTop ?
+						document.documentElement.scrollTop :
+						document.body.scrollTop;
+				var scrollLeft = document.documentElement.scrollLeft ?
+						document.documentElement.scrollLeft :
+						document.body.scrollLeft;
+				var elementLeft = rect.left+scrollLeft;
+				var elementTop = rect.top+scrollTop;
+	
+				mouseX = evt.pageX-elementLeft;
+				mouseY = evt.pageY-elementTop;	
+			}
+	
+			function draw() {
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				mask.draw(ctx);
+			}
+	
+			function mouseDownListener(evt) {
+				updateMouse(evt, canvas);
+				if(dirScope.config.mode === 'edit' && controller.startDrag(mouseX, mouseY)) {
+					canvas.addEventListener('mousemove', mouseEditMoveListener, false);
+				}
+	
+				// Prevent event going further
+				if (evt.preventDefault) {
+					evt.preventDefault();
+				}
+				else if (evt.returnValue) {
+					evt.returnValue = false;
+				}
+			}
+	
+			function mouseEditMoveListener(evt) {
+				if(controller.getDragging()) {
+					updateMouse(evt, canvas);
+					console.log('starting to drag');
+					controller.drag(mouseX, mouseY);
+					console.log('stopping drag and starting draw');
+					draw();
+					console.log('stopping draw');
+				}
+			}
+	
+			function mouseUpListener(evt) {
+				updateMouse(evt, canvas);
+				controller.stopDrag(mouseX, mouseY);
+				canvas.removeEventListener('mousemove', mouseEditMoveListener, false);
+				dirScope.$apply();
+			}
+
 			dirScope = scope;
 			canvas = element[0];
 			ctx = canvas.getContext('2d');
